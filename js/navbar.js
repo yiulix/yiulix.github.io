d98d341b6597ea909a2ef9e492e0031d670d1c02
yiulix <gzhuaa@ust.hk>
2018-02-11 00:59:36 -0500

    0.0

---
+++ js/navbar.js
@@ -0,0 +1,22 @@
+(function($) {
+    "use strict";
+
+    $('body').scrollspy({
+        target: '#mainNav',
+        offset: 80
+    });
+
+    var navbarCollapse = function () {
+        if ($("#mainNav").offset().top > 100) {
+            $("#mainNav").addClass("navbar-shrink");
+        } else {
+            $("#mainNav").removeClass("navbar-shrink");
+        }
+    };
+    // Collapse now if page is not at top
+    navbarCollapse();
+    // Collapse the navbar when page is scrolled
+    $(window).scroll(navbarCollapse);
+
+
+})(jQuery);
